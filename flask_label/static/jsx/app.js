632338f94cfdcacc506b964bdc236bb523d0778f
Tobias Gottwald <tobias.gottwald@gastrobotics.com>
2018-06-12 14:40:10 +0200

    Create my own little routing hack

--- flask_label/static/jsx/app.js
+++ flask_label/static/jsx/app.js
@@ -1,1 +1,22 @@
-console.log("derpaherpa");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ExtremeClicking from "./ExtremeClicking.jsx";
+
+
+// #MyHackRouter
+var doc_root = document.getElementById("react_root");
+var attribute = doc_root.getAttribute("react-page");
+if (attribute === "label_interface") {
+    ReactDOM.render(
+        <Clock />,
+        doc_root
+    );
+}
+else if (attribute === "other_attribute") {
+    ReactDOM.render(
+        <h1>Placeholder</h1>,
+        doc_root
+    );
+}
+
