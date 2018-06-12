987e75e8dd98a90bb2711d20945f77525adad232
Tobias Gottwald <tobias.gottwald@gastrobotics.com>
2018-06-12 19:51:09 +0200

    Update api to sqlalch

--- flask_label/api.py
+++ flask_label/api.py
@@ -4,7 +4,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, current_app, send_from_directory
 )
 from flask_label.auth import api_login_required
-from flask_label.database import db
+from flask_label.models import ImageTask
 
 bp = Blueprint("api", __name__,
                 url_prefix="/api")
@@ -22,19 +22,13 @@ def serve_image(img_id):
     Args:
         img_id (int): Is the same as task id, since every task is matched to one image.
     """
-    img_tasks = db.execute(
-        "SELECT it.filename, b.dirname "
-        "FROM image_task it "
-        "INNER JOIN image_batch b ON b.id = it.batch_id "
-        "WHERE it.id = ? ",
-        (img_id,)
-    ).fetchone()
+    img_task = ImageTask.query.filter_by(id=img_id).first()
 
     img_path = os.path.join(
         current_app.instance_path,
         current_app.config["IMAGE_DIR"],
-        img_tasks["dirname"]
+        img_task.batch.dirname
     )
 
-    current_app.logger.info(os.path.join(img_path, img_tasks["filename"]))
-    return send_from_directory(img_path, img_tasks["filename"])
+    current_app.logger.info(os.path.join(img_path, img_task.filename))
+    return send_from_directory(img_path, img_task.filename)
