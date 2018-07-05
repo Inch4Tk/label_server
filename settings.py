4bfc7bb26217c0b6a4e3d8ac9112a12eec265d03
Christian Scherer <scherer.christian2812@gmail.com>
2018-07-05 17:01:03 +0200

    Move class_id file in separate directory

--- settings.py
+++ settings.py
@@ -2,5 +2,6 @@ import os
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 annotation_predictor_dir = os.path.join(root_dir, 'annotation_predictor')
+annotation_predictor_metadata_dir = os.path.join(annotation_predictor_dir, 'metadata')
 
-class_id_file = os.path.join(annotation_predictor_dir, 'class_ids.json')
+class_id_file = os.path.join(annotation_predictor_metadata_dir, 'class_ids.json')
