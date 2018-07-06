acf99c3f5b302572b350f98f90b6fec7709d36ba
Tobias Gottwald <tobias.gottwald@gastrobotics.com>
2018-07-06 13:09:17 +0200

    Implement Breadcrumbs

--- flask_label/static/jsx/TopBar.jsx
+++ flask_label/static/jsx/TopBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, Route } from "react-router-dom"
+import { Breadcrumb } from "./Breadcrumb.jsx"
 
 class UserInfo extends React.Component {
     render() {
@@ -11,14 +12,26 @@ class UserInfo extends React.Component {
     }
 }
 
+class Logo extends React.Component {
+    render() {
+        return (
+            <div className="logo">
+                <Link to="/">Flask Label</Link>
+            </div>
+        )
+    }
+}
+
+
 class Navigation extends React.Component {
     render() {
-        const navPills = this.props.navPills ? this.props.navPills : [];
+        // Dynamic = Things like instructions/settings, context aware
         const navDynamic = this.props.navDynamic ? this.props.navDynamic : [];
         return (
             <div className="navigation">
-                <nav className="nav-pills">
-                    {navPills.map((pill, index) => (<Link to={pill.link} key={index}>{pill.name}</Link>))}
+                <nav className="nav-breadcrumbs">
+                    {/* Start our nav breadcrumbs on the first level, not root. */}
+                    <Route path={"/:name"} component={Breadcrumb}/>
                 </nav>
                 <nav className="nav-dynamic">
                     {navDynamic.map((dyn, index) => (<Link to={dyn.link} key={index}>{dyn.name}</Link>))}
@@ -32,10 +45,11 @@ class TopBar extends React.Component {
     render() {
         return (
             <div className="top-bar">
-                <Navigation navPills={[{link: "/test", name: "test"}, {link: "/derp", name: "derp"}]}/>
+                <Logo />
+                <Navigation />
                 <UserInfo />
             </div>
-        );
+        )
     }
 }
 
--- flask_label/static/jsx/app.js
+++ flask_label/static/jsx/app.js
@@ -2,9 +2,9 @@
 import "./../scss/app.scss"
 
 // JS Assets
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 import ExtremeClicking from "./ExtremeClicking.jsx";
 import { TopBar } from "./TopBar.jsx";
@@ -12,14 +12,14 @@ import { TopBar } from "./TopBar.jsx";
 const NoMatch = () => (
     <h1>404: Could not find route</h1>
 )
-
+//[{link: "/todo", name: "Instructions"}, {link: "/todo", name: "Settings"}]
 const App = () => (
     <div>
         <TopBar />
 
         <Switch>
-            <Route exact path='/' render={()=>(<h1>Home</h1>)}/>
-            <Route exact path='/test' render={() => (<h1>Test</h1>)} />
+            <Route exact path="/" render={() => (<h1>Home</h1>)} />
+            <Route exact path="/todo" render={() => (<h1>TODO</h1>)} />
             <Route component={NoMatch}/>
         </Switch>
     </div>
