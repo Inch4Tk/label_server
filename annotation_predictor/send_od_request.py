49bedb167f7285e794c6039da02794c1e87c0ce7
Christian Scherer <scherer.christian2812@gmail.com>
2018-07-11 15:14:28 +0200

    Add function that sends a detection request to a tf server
    * assumes that model is available on port 9000 under the name 'mobile_v2'

---
+++ annotation_predictor/send_od_request.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from grpc.beta import implementations
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2
+
+def load_image(path_to_image: str):
+    img = Image.open(path_to_image)
+    img.load()
+    data = np.asarray(img, dtype='uint8')
+    data = np.expand_dims(data, 0)
+    return data
+
+def send_od_request(path_to_image: str):
+    channel = implementations.insecure_channel('localhost', 9000)
+
+    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+
+    request = predict_pb2.PredictRequest()
+
+    request.model_spec.name = 'mobile_v2'
+    request.model_spec.signature_name = 'serving_default'
+
+    img = load_image(path_to_image)
+    request.inputs['inputs'].CopyFrom(tf.make_tensor_proto(img, dtype=tf.uint8))
+
+    pred_result = stub.Predict(request, 60.0)
+
+    bounding_boxes = pred_result.outputs['detection_boxes'].float_val
+    classes = pred_result.outputs['detection_classes'].float_val
+    scores = pred_result.outputs['detection_scores'].float_val
+
+    filename = os.path.basename(path_to_image)
+    image_id = os.path.splitext(filename)[0]
+
+    result = {image_id: []}
+
+    for i, cls in enumerate(classes):
+        confidence = scores[i]
+
+        if confidence == 0.0:
+            break
+
+        label_name = cls
+        ymin = bounding_boxes[4 * i]
+        xmin = bounding_boxes[4 * 1 + 1]
+        ymax = bounding_boxes[4 * 1 + 2]
+        xmax = bounding_boxes[4 * 1 + 3]
+        result[image_id].append({'LabelName': label_name, 'Confidence': confidence,
+                                 'XMin': xmin, 'YMin': ymin, 'XMax': xmax, 'YMax': ymax})
+
+    return result
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit(-1)
+    result = send_od_request(sys.argv[1])
+    print(result)
