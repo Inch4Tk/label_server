451c4bb93a58c98b5171af2609f4b82dfa6de95b
Christian Scherer <scherer.christian2812@gmail.com>
2018-07-16 16:32:03 +0200

    Add test for new functionality of GroundTruthReader.py
    * Should return empty list when image_id is not found

--- annotation_predictor/test/test_groundtruthreader.py
+++ annotation_predictor/test/test_groundtruthreader.py
@@ -6,6 +6,11 @@ def test_get_groundtruth_annotation_finds_annotation_for_image():
         ret = test_ground_truth_reader.get_ground_truth_annotation('test_id')
     assert ret == test_data
 
+def test_returns_empty_list_for_unknown_id():
+    with GroundTruthReader(test_file) as test_ground_truth_reader:
+        ret = test_ground_truth_reader.get_ground_truth_annotation('nonexistent')
+    assert ret == []
+
 def test_parse_to_dict():
     with GroundTruthReader(test_file) as test_ground_truth_reader:
         assert test_ground_truth_reader.gt_dict == test_dict
