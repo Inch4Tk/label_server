3fe88f65ce6c6594633a189b009a1a5f3e6fe9f1
Christian Scherer <scherer.christian2812@gmail.com>
2018-07-16 16:26:21 +0200

    Dynamically update detection_record-json
    * add progress notification while at it

--- annotation_predictor/create_detection_record.py
+++ annotation_predictor/create_detection_record.py
@@ -8,18 +8,25 @@ from settings import annotation_predictor_metadata_dir
 
 def create_detection_record(path_to_images: str):
     result = {}
-    for image in os.listdir(path_to_images):
-        path_to_image = os.path.join(path_to_images, image)
-        result.update(send_od_request(path_to_image))
-
+    total_images = len(os.listdir(path_to_images))
     path_to_json = os.path.join(annotation_predictor_metadata_dir,
                                 '{}.json'.format(datetime.now().strftime('%Y_%m_%d_%H%M%S')))
+
     with open(path_to_json, 'w') as f:
         json.dump(result, f)
 
+    for i, image in enumerate(os.listdir(path_to_images)):
+        print('Evaluated {} of {} images'.format(i, total_images))
+        path_to_image = os.path.join(path_to_images, image)
+        result.update(send_od_request(path_to_image))
+        with open(path_to_json, 'r+') as f:
+            json.dump(result, f)
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Create and save a detection record for a set of images')
-    parser.add_argument('path_to_images', type=str, metavar='path_to_images', help='path to training images')
+    parser = argparse.ArgumentParser(
+        description='Create and save a detection record for a set of images')
+    parser.add_argument('path_to_images', type=str, metavar='path_to_images',
+                        help='path to training images')
     args = parser.parse_args()
 
     create_detection_record(args.path_to_images)
