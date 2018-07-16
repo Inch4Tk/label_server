7d608acfabf10ee3686d3b408d6d69366075733f
Christian Scherer <scherer.christian2812@gmail.com>
2018-07-16 16:17:44 +0200

    Refactor
    Fusion util-functionality in one python file

--- annotation_predictor/test/test_util.py
+++ annotation_predictor/test/test_util.py
@@ -1,6 +1,9 @@
 from collections import OrderedDict
 
-from annotation_predictor.compute_iou import compute_iou
+import numpy as np
+
+from annotation_predictor.test.conftest import test_data
+from annotation_predictor.util import compute_iou, compute_feature_vector
 
 def test_compute_iou_for_non_overlap():
     box1 = OrderedDict([('XMin', '0.1'), ('XMax', '0.2'), ('YMin', '0.1'), ('YMax', '0.2')])
@@ -19,3 +22,11 @@ def test_compute_iou_for_partial_overlap():
     box2 = OrderedDict([('XMin', '0.3'), ('XMax', '0.5'), ('YMin', '0.6'), ('YMax', '0.8')])
     result = compute_iou(box1, box2)
     assert result == 0.04
+
+def test_compute_feature_vector():
+    feature_vector = compute_feature_vector(test_data, 1)
+    expected = [0.5, 0.01, 0.3, 0.2, 0.0]
+    expected_one_hot_enc = np.zeros(601)
+    expected_one_hot_enc[459] = 1
+    expected.extend(expected_one_hot_enc)
+    assert feature_vector == expected
