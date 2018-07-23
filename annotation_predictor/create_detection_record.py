8eab9ddc0d29a373fbac40997cd2cc95a2057089
Christian Scherer <scherer.christian2812@gmail.com>
2018-07-23 18:49:46 +0200

    Make create_detection_record more flexible
    * Can now pause and later continue a prediction by giving the path to an existing json-file

--- annotation_predictor/create_detection_record.py
+++ annotation_predictor/create_detection_record.py
@@ -6,18 +6,31 @@ from datetime import datetime
 from annotation_predictor.send_od_request import send_od_request
 from settings import annotation_predictor_metadata_dir
 
-def create_detection_record(path_to_images: str):
-    result = {}
-    total_images = len(os.listdir(path_to_images))
-    path_to_json = os.path.join(annotation_predictor_metadata_dir,
-                                '{}.json'.format(datetime.now().strftime('%Y_%m_%d_%H%M%S')))
+def create_detection_record(path_to_images: str, path_to_json=None):
+    if path_to_json is None:
+        images = os.listdir(path_to_images)
+        result = {}
+        timestamp = datetime.now().strftime('%Y_%m_%d_%H%M%S')
+        path_to_json = os.path.join(annotation_predictor_metadata_dir, '{}.json'.format(timestamp))
+        with open(path_to_json, 'w') as f:
+            json.dump(result, f)
 
-    with open(path_to_json, 'w') as f:
-        json.dump(result, f)
+    else:
+        with open(path_to_json, 'r') as f:
+            result = json.load(f)
+            images = []
+        for image in os.listdir(path_to_images):
+            image_id = os.path.splitext(image)[0]
+            if image_id not in result.keys():
+                images.append(image)
 
-    for i, image in enumerate(os.listdir(path_to_images)):
-        print('Evaluated {} of {} images'.format(i, total_images))
+    total_images = len(images)
+
+    for i, image in enumerate(images):
+        if (i % 100) == 0:
+            print('Evaluated {} of {} images'.format(i, total_images))
         path_to_image = os.path.join(path_to_images, image)
+
         result.update(send_od_request(path_to_image))
         with open(path_to_json, 'r+') as f:
             json.dump(result, f)
@@ -27,6 +40,8 @@ if __name__ == '__main__':
         description='Create and save a detection record for a set of images')
     parser.add_argument('path_to_images', type=str, metavar='path_to_images',
                         help='path to training images')
+    parser.add_argument('--path_to_json', type=str, metavar='path_to_json',
+                        help='path to exising json file which will be updated', required=False)
     args = parser.parse_args()
 
-    create_detection_record(args.path_to_images)
+    create_detection_record(args.path_to_images, args.path_to_json)
