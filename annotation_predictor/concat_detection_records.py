a4ee0468014a9de5cfdec209b13ceeb11d664c6c
Christian Scherer <scherer.christian2812@gmail.com>
2018-07-23 18:50:08 +0200

    Utility function to concatenate two json-files

---
+++ annotation_predictor/concat_detection_records.py
@@ -0,0 +1,31 @@
+import argparse
+import json
+import os
+from datetime import datetime
+
+from settings import annotation_predictor_metadata_dir
+
+def concat_detection_record(record1: str, record2: str):
+    timestamp = datetime.now().strftime('%Y_%m_%d_%H%M%S')
+    filename = '{}.json'.format(timestamp)
+    path_to_file = os.path.join(annotation_predictor_metadata_dir, filename)
+    with open(record1, 'r') as f:
+        r1 = json.load(f)
+    with open(record2, 'r') as f:
+        r2 = json.load(f)
+
+    r1.update(r2)
+
+    with open(path_to_file, 'w') as f:
+        json.dump(r1, f)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Concatenate two detection records')
+    parser.add_argument('path_to_record_1', type=str, metavar='path_to_record_1',
+                        help='path to first training record')
+    parser.add_argument('path_to_record_2', type=str, metavar='path_to_record_2',
+                        help='path to second training record')
+    args = parser.parse_args()
+
+    concat_detection_record(args.path_to_record_1, args.path_to_record_2)
