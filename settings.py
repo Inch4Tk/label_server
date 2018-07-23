f0ca4989b7e94ef27a202a174a3efda6dd6ed16d
Christian Scherer <scherer.christian2812@gmail.com>
2018-07-23 18:43:44 +0200

    Add path to model directory

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import os
 root_dir = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(root_dir, 'instance', 'images')
 annotation_predictor_dir = os.path.join(root_dir, 'annotation_predictor')
+model_dir = os.path.join(annotation_predictor_dir, 'models')
 annotation_predictor_metadata_dir = os.path.join(annotation_predictor_dir, 'metadata')
 
 class_ids_oid_file = os.path.join(annotation_predictor_metadata_dir, 'class_ids_oid.json')
