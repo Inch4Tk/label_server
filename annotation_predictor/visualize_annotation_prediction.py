7ad1af558e344366c46d5b4c710bd63e31aefcc2
Christian Scherer <scherer.christian2812@gmail.com>
2018-07-30 18:34:51 +0200

    Adapt visualization function
    * Takes path to an image and
    * shows the annotation prediction with its corresponding bounding box

---
+++ annotation_predictor/visualize_annotation_prediction.py
@@ -0,0 +1,39 @@
+import argparse
+import json
+import os
+
+from PIL import Image, ImageDraw
+
+from annotation_predictor import accept_prob_predictor
+from annotation_predictor.send_od_request import send_od_request
+from annotation_predictor.settings import class_ids_oid_file
+
+def visualize_detection(path_to_image: str):
+    with open(class_ids_oid_file) as f:
+        class_ids_oid = json.load(f)
+    det = send_od_request(path_to_image)
+    pred = accept_prob_predictor.main('predict', det)
+    image_id = os.path.splitext(os.path.basename(path_to_image))[0]
+    img = Image.open(path_to_image)
+    colors = ['blue', 'green', 'red', 'orange', 'brown', 'black', 'turquoise']
+    width, height = img.size
+    draw = ImageDraw.Draw(img)
+    for i, det in enumerate(det[image_id]):
+        color = colors[i % len(colors)]
+        x_min = det['XMin'] * width
+        y_min = det['YMin'] * height
+        x_max = det['XMax'] * width
+        y_max = det['YMax'] * height
+        draw.text(xy=(x_min, y_min), text=class_ids_oid[det['LabelName']], fill=color)
+        draw.text(xy=((x_min+x_max)/2, (y_min+y_max)/2), text=str(int(pred[i])), fill=color)
+        for offset in [-1, 0, 1]:
+            draw.rectangle(xy=[x_min + offset, y_min + offset, x_max + offset, y_max + offset],
+                           outline=color)
+    img.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train a proposal network for labeling tasks')
+    parser.add_argument('path_to_image', type=str, metavar='path_to_image', help='path to image')
+
+    args = parser.parse_args()
+    visualize_detection(args.path_to_image)
