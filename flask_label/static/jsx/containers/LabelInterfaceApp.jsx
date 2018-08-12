e6c15abf00263f144cdcfd8cd355192b9a6be6de
Christian Scherer <scherer.christian2812@gmail.com>
2018-08-12 23:01:31 +0200

    Add key to props of LabelInterface
    * this is done to force the LabelInterface to remount when the url-path is changed
    ** thus ensuring that the correct image is loaded

--- flask_label/static/jsx/containers/LabelInterfaceApp.jsx
+++ flask_label/static/jsx/containers/LabelInterfaceApp.jsx
@@ -2,25 +2,24 @@ import {connect} from "react-redux";
 import {LabelInterface} from "../components/LabelInterface.jsx";
 
 function getBatchWithId(state, id) {
-    const batch = state.batches.imageBatches.find(x => x.id == id);
-    return batch;
+    return state.batches.imageBatches.find(x => x.id == id);
 }
 
 function getTaskWithId(state, batch, id) {
-    const task = state.batches.imageBatches.find(x => x.id == batch).tasks.find(x => x.id == id);
-    return task;
+    return state.batches.imageBatches.find(x => x.id == batch).tasks.find(x => x.id == id);
 }
 
 const mapStateToProps = (state, {match} ) => {
     return {
+        key: match.params.task_id,
         batch: getBatchWithId(state, match.params.batch_id),
         task: getTaskWithId(state, match.params.batch_id, match.params.task_id)
     };
-}
+};
 
 const mapDispatchToProps = dispatch => ({
     //toggleTodo: id => dispatch(toggleTodo(id))
-})
+});
 
 // Tie state and visualization together
 export default connect(
