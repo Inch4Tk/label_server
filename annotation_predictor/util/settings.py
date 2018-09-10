b8200d30243d74cadf6304674de2b5a4ccc8f502
Christian Scherer <scherer.christian2812@gmail.com>
2018-09-10 11:58:43 +0200

    Move model-dir to root in order
    * easier access from docker
    * makes more sense as it also contains the object detection model

--- annotation_predictor/util/settings.py
+++ annotation_predictor/util/settings.py
@@ -1,9 +1,10 @@
 import os
 
-annotation_predictor_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+annotation_predictor_dir = os.path.join(root_dir, 'annotation_predictor')
 annotation_predictor_metadata_dir = os.path.join(annotation_predictor_dir, 'metadata')
 annotation_predictor_util_dir = os.path.join(annotation_predictor_dir, 'util')
-model_dir = os.path.join(annotation_predictor_dir, 'models')
+model_dir = os.path.join(root_dir, 'models')
 
 class_ids_oid_file = os.path.join(annotation_predictor_metadata_dir, 'class_ids_oid.json')
 class_ids_coco_file = os.path.join(annotation_predictor_metadata_dir, 'class_ids_coco.json')
