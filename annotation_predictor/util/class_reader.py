2f1964a41708c7269ad304761595cd1a6662c6d8
Christian Scherer <scherer.christian2812@gmail.com>
2018-09-26 20:24:47 +0200

    Add new functionality to class_readder
    * get index (key) of a class from a label (value)
    * make it possible to extend the dict of classes

--- annotation_predictor/util/class_reader.py
+++ annotation_predictor/util/class_reader.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 class ClassReader:
     def __init__(self, class_id_file):
@@ -7,8 +8,12 @@ class ClassReader:
         Args:
             class_id_file: File which contains information about available classes in a dataset.
         """
-        with open(class_id_file) as file:
-            self.class_ids = json.load(file)
+        self.class_id_file = class_id_file
+        if os.path.exists(class_id_file):
+            with open(class_id_file) as file:
+                self.class_ids = json.load(file)
+        else:
+            self.class_ids = {}
 
     def get_class_from_id(self, id: str) -> str:
         """
@@ -33,6 +38,20 @@ class ClassReader:
         """
         return list(self.class_ids.keys()).index(id)
 
+    def get_index_of_class_from_label(self, label: str) -> int:
+        """
+        Returns the unique index of a class used for defining a one-hot-encoding for the class.
+
+        Args:
+            label: human-readable label of a class
+
+        Returns: Index of the class in the class-id file.
+        """
+        if label not in self.class_ids.values():
+            return -1
+
+        return list(self.class_ids.values()).index(label)
+
     def get_key_of_class_from_label(self, label: str) -> int:
         """
         Returns the unique index of a class used for defining a one-hot-encoding for the class.
@@ -45,3 +64,15 @@ class ClassReader:
         keys = list(self.class_ids.keys())
         values = list(self.class_ids.values())
         return keys[values.index(label)]
+
+    def add_class_to_file(self, cls: str):
+        """
+        Adds a new class to the file containing the known classes
+
+        Args:
+            cls: name of new class.
+        """
+        new_class_key = len(self.class_ids) + 1
+        self.class_ids[new_class_key] = cls
+        with open(self.class_id_file, 'w') as file:
+            json.dump(self.class_ids, file)
